d6b8c930d1802ddc519f142d8533ef11125fc940
Nigel Whatling <nigelw@n-vision.com.au>
2019-08-27 23:13:34 +1000

    Started added some controls. Well... a drawer to put them in at least.

--- src/components/SideDrawer.js
+++ src/components/SideDrawer.js
@@ -1,10 +1,51 @@
 import React, { useState, useRef, useEffect } from 'react';
-import {Drawer } from '@material-ui/core';
+import { Drawer } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import Fab from '@material-ui/core/Fab';
 
-const SideDrawer = ({ width, height }) => {
+const useStyles = makeStyles(theme => ({
+    fab: {
+        position: 'absolute',
+        left: 10,
+        top: 10,
+        margin: theme.spacing(1),
+    },
+    content: {
+        width: 250
+    }
+}));
+
+const SideDrawer = () => {
+
+    const classes = useStyles();
+    const [state, setState] = useState(false);
+
+    const toggleDrawer = (open) => event => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState(open);
+    };
 
     return (
-        <Drawer anchor="left" open={false} ></Drawer>
+        <div>
+            <Fab size="small" color="primary" aria-label="menu" className={classes.fab} onClick={toggleDrawer(true)}>
+                <MenuIcon />
+            </Fab>
+
+            <Drawer anchor="left" open={state} onClose={toggleDrawer(false)}>
+                <div
+                    className={classes.content}
+                    role="presentation"
+                    onClick={toggleDrawer(false)}
+                    onKeyDown={toggleDrawer(false)}
+                >
+                    controls go here
+                </div>
+            </Drawer>
+        </div>
     );
 }
 
